fc7896343ecf008ef90dbf1880c0ce4a39312c4d
Stefan Wick <wick.stefan@outlook.com>
2017-04-16 21:18:17 -0700

    changes in comments only

--- ShapeRecognitionUWP/MainPage.xaml.cs
+++ ShapeRecognitionUWP/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,13 +20,8 @@ using Windows.UI.Input;
 using Windows.UI.Input.Inking;
 using Windows.UI.Input.Inking.Analysis;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace ShapeRecognitionUWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         InkAnalyzer inkAnalyzer = null;
